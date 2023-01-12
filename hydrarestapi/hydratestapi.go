54a680e8d0ec82e53eda85eab2a6206daf5b8a93
Hendrik Pienaar <pienaahj@gmail.com>
2023-01-12 13:08:16 +0200

    name change for hydrarestapi

--- hydrarestapi/hydratestapi.go
+++ hydrarestapi/hydratestapi.go
@@ -12,22 +12,27 @@ type DBlayerconfig struct {
 	Conn string `json:"connectionstring"`
 }
 
-func InitializeAPIHandlers() {
+func InitializeAPIHandlers() error {
 	conf := new(DBlayerconfig)
 	err := hydraConfigurator.GetConfiguration(hydraConfigurator.JSON, conf, "/hydraweb/apiconfig.json")
 	if err != nil {
-		log.Fatal("Error decoding JSON, err")
+		log.Println("Error decoding JSON, err")
+		return err
 	}
 	h := NewhydraCrewReqHandler()
 	err = h.connect(conf.DB, conf.Conn)
 	if err != nil {
-		log.Fatal("Error connecting to db ", err)
+		log.Println("Error connecting to db ", err)
+		return err
 	}
 	http.HandleFunc("/hydracrew/", h.handleHydraCrewRequests)
 	// http.handle("/hydracrew/", h) if using ServeHTTP
+	return nil
 }
 
-func RunAPI() {
-	InitializeAPIHandlers()
-	http.ListenAndServe(":8061", nil)
+func RunAPI() error {
+	if err := InitializeAPIHandlers(); err != nil {
+		return err
+	}
+	return http.ListenAndServe(":8061", nil)
 }
